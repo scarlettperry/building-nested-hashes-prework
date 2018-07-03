6d22a5dd0dc10afc9a04d11c55db98cebb6a0304
Scarlett Perry <si.perry15@gmail.com>
2018-07-03 15:23:33 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -20,7 +20,10 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {},
+        {:name => "",
+          :age => "",
+          :attitude => ""
+        },
         {}
       ]
    }
