9be593c5c78294d5149a5665b4ac887c22878133
Scarlett Perry <si.perry15@gmail.com>
2018-07-03 15:13:55 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,11 +2,11 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague"
+        :name => "Lord Montague",
         :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => ""
         },
       :hero => {
         #your key/value pairs here
